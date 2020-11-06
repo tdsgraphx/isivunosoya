cd5e91f2e2521e6de8cb4a29d2ec914fedba510a
TDS Graphics <54712534+tdsgraphx@users.noreply.github.com>
2020-11-06 11:57:39 +0200

    Add files via upload
    initial commit

---
+++ js/side_nav.js
@@ -0,0 +1,47 @@
+﻿function openHomeNav() {
+    document.getElementById("home_sidenav").style.width = "200px";
+}
+
+function closeHomeNav() {
+    document.getElementById("home_sidenav").style.width = "0";
+}
+
+/* ===================================================================== */
+
+function openAboutNav() {
+    document.getElementById("about_sidenav").style.width = "200px";
+}
+
+function closeAboutNav() {
+    document.getElementById("about_sidenav").style.width = "0";
+}
+
+/* ===================================================================== */
+
+function openServicesNav() {
+    document.getElementById("services_sidenav").style.width = "200px";
+}
+
+function closeServicesNav() {
+    document.getElementById("services_sidenav").style.width = "0";
+}
+
+/* ===================================================================== */
+
+function openContactsNav() {
+    document.getElementById("contacts_sidenav").style.width = "200px";
+}
+
+function closeContactsNav() {
+    document.getElementById("contacts_sidenav").style.width = "0";
+}
+
+/* ===================================================================== */
+
+function openIndexNav() {
+    document.getElementById("index_sidenav").style.width = "200px";
+}
+
+function closeIndexNav() {
+    document.getElementById("index_sidenav").style.width = "0";
+}
